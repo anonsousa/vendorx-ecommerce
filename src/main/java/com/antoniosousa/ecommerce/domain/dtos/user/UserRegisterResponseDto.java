bae8ebb6f863c94f2ed9810b041eb4b3e070d219
Antonio <pisouza011@gmail.com>
2024-09-22 14:56:37 -0300

    feat: First Service from User (create)

--- src/main/java/com/antoniosousa/ecommerce/domain/dtos/user/UserRegisterResponseDto.java
+++ src/main/java/com/antoniosousa/ecommerce/domain/dtos/user/UserRegisterResponseDto.java
@@ -1,12 +1,18 @@
 package com.antoniosousa.ecommerce.domain.dtos.user;
 
 import com.antoniosousa.ecommerce.domain.entities.enums.AccountStatus;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserRegisterResponseDto {
+
+    private Long id;
     private String username;
     private String email;
     private String password;
