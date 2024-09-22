a83bc0348bf8e6a470a22d93bf27c23cefa173ba
Antonio <pisouza011@gmail.com>
2024-09-22 19:14:23 -0300

    feat: Application Exceptions Handler and customs exceptions

---
+++ src/main/java/com/antoniosousa/ecommerce/infra/exceptions/ApplicationExceptionHandler.java
@@ -0,0 +1,47 @@
+package com.antoniosousa.ecommerce.infra.exceptions;
+
+import jakarta.validation.ConstraintViolationException;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@ControllerAdvice
+public class ApplicationExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        List<FieldError> fieldErrors = ex.getBindingResult().getFieldErrors();
+
+        for (FieldError fieldError : fieldErrors) {
+            errors.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+
+        return errors;
+    }
+
+    @ResponseStatus(HttpStatus.CONFLICT)
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public Map<String, String> handleDataIntegrityViolationException(DataIntegrityViolationException ex) {
+        Map<String, String> errors = new HashMap<>();
+
+        if (ex.getCause() instanceof ConstraintViolationException) {
+            errors.put("Error", "Duplicate Key Error!");
+            errors.put("message", ex.getCause().getMessage());
+        } else {
+            errors.put("Error", "Something went wrong!");
+            errors.put("message", ex.getCause().getMessage());
+        }
+        return errors;
+    }
+
+}
